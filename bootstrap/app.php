56fa809ff39671f04a49b215bf5e20291637a0d1
jalis <jalismahamud31@gmail.com>
2025-12-25 16:47:14 +0600

    added admin route and controller logic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'redirectAdmin' => \App\Http\Middleware\redirectAdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
