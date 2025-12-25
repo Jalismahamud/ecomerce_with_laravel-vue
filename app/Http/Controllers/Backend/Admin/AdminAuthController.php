1e8fc89b78a59d140504319a36200235532e42d2
jalis <jalismahamud31@gmail.com>
2025-12-25 18:54:55 +0600

    Refactor admin logout route name for consistency

--- app/Http/Controllers/Backend/Admin/AdminAuthController.php
+++ app/Http/Controllers/Backend/Admin/AdminAuthController.php
@@ -16,13 +16,16 @@ class AdminAuthController extends Controller
 
     public function login(Request $request)
     {
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'isAdmin' == true])) {
-            return redirect()->route('admin.dashboard')->with('error', 'Invalid Credentials. Try Againg.');
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password, 'isAdmin' => true])) {
+            return redirect()->back()->with('error', 'Invalid Credentials. Try Again.');
         }
+
+        return redirect()->route('admin.dashboard')->with('success', 'Login successful.');
     }
 
     public function logout(Request $request)
     {
+        dd('runnig');
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- Navbar Start -->
+        <Navbar />
+        <!-- Navbar End -->
+        <!-- Sidebar -->
+        <Sidebar />
+        <!-- Sidebar End -->
+
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Sidebar from './Sidebar.vue'
+import Navbar from './Navbar.vue'
+
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
